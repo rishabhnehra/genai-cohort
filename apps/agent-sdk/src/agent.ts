74f1ebf10c7ae3d2dd8720b441e920cf19411be9
rishabhnehra <rishabhnehrapersonal@gmail.com>
2026-08-03 23:29:34 +0530

    feat: tools and its implimentation

--- apps/agent-sdk/src/agent.ts
+++ apps/agent-sdk/src/agent.ts
@@ -1,15 +1,19 @@
+import "dotenv/config";
 import { HARNESS_PROMPT } from "./config.js";
+import OpenAI from "openai";
 
 interface Message {
-  role: "USER" | "ASSISTANT" | "DEVELOPER";
+  role: "user" | "assistant" | "developer";
   content: string;
 }
 
-interface Tool {
+export interface Tool {
   name: string;
   description: string;
   executor: (input: string) => Promise<string>;
 }
+
+type Interceptor = (message: Message) => void;
 
 class AgentBuilder {
   public instructions: string | undefined;
@@ -27,6 +31,7 @@ class AgentBuilder {
 
   public tool(t: Tool) {
     this.tools.push(t);
+    return this;
   }
 
   public build() {
@@ -38,9 +43,18 @@ export class Agent {
   private instructions: string | undefined;
   private messages: Message[];
   private toolMap: Map<string, Tool>;
+  private openAi: OpenAI;
+  private interceptors: Interceptor[];
+
+  private MAX_LOOP = 50;
 
   constructor(builder: AgentBuilder) {
+    this.interceptors = [];
     this.toolMap = new Map();
+    this.openAi = new OpenAI({
+      apiKey: process.env.OpenAI,
+      baseURL: "https://openrouter.ai/api/v1",
+    });
 
     for (const t of builder.tools) {
       this.toolMap.set(t.name, t);
@@ -62,11 +76,86 @@ export class Agent {
     return new AgentBuilder();
   }
 
-  public getInstruction() {
-    return this.instructions;
+  public attachInterceptor(interceptor: Interceptor) {
+    this.interceptors.push(interceptor);
   }
 
-  public run() {
-    return `From Agent class -> Instruction ${this.instructions}`;
+  private notifyInterceptor(message: Message) {
+    for (const interceptor of this.interceptors) {
+      interceptor(message);
+    }
+  }
+
+  public getInstruction() {
+    return this.instructions || "";
+  }
+
+  public async run(query: string) {
+    this.messages.push({
+      role: "user",
+      content: query,
+    });
+    for (let i = 0; i < this.MAX_LOOP; i++) {
+      const llmResponse = await this.openAi.chat.completions.create({
+        model: "gpt-4o",
+        messages: [
+          { role: "system", content: this.getInstruction() },
+          ...this.messages.map((message) => ({
+            role: message.role,
+            content: message.content,
+          })),
+        ],
+      });
+
+      const rawLlmResponse = llmResponse.choices[0]?.message.content as string;
+
+      this.notifyInterceptor({
+        role: "assistant",
+        content: rawLlmResponse,
+      });
+      this.messages.push({
+        role: "assistant",
+        content: rawLlmResponse,
+      });
+
+      const parsedResponse = JSON.parse(rawLlmResponse);
+
+      if (parsedResponse.step.toLowerCase() === "output") {
+        return this.messages;
+      }
+
+      if (parsedResponse.step.toLowerCase() === "tool_request") {
+        const { functionName, input } = parsedResponse;
+
+        if (!this.toolMap.has(functionName)) {
+          this.messages.push({
+            role: "developer",
+            content: "Error: Tool not found",
+          });
+          continue;
+        }
+
+        const toolResult = await this.toolMap
+          .get(functionName)
+          ?.executor(input);
+
+        this.notifyInterceptor({
+          role: "developer",
+          content: JSON.stringify({
+            functionName,
+            input,
+            toolResult,
+          }),
+        });
+        this.messages.push({
+          role: "developer",
+          content: JSON.stringify({
+            functionName,
+            input,
+            toolResult,
+          }),
+        });
+      }
+    }
   }
 }
--- apps/agent-sdk/src/index.ts
+++ apps/agent-sdk/src/index.ts
@@ -1,14 +1,72 @@
-import { Agent } from "./agent.js";
+import { Agent, type Tool } from "./agent.js";
+import { promisify } from "node:util";
+import { exec as execCallback } from "node:child_process";
+
+const exec = promisify(execCallback);
+
+const weatherTool: Tool = {
+  name: "fetchWeather",
+  description: "Tool that returns weather data of given input string",
+  async executor(city: string) {
+    const result = await fetch(
+      `https://wttr.in/${city.toLowerCase()}?format=%C+%t`,
+    ).then((res) => res.text());
+
+    return JSON.stringify({ city, weather: result });
+  },
+};
+
+const cliAccessTool: Tool = {
+  name: "cliAccess",
+  description: "Access to CLI commands",
+  async executor(command: string) {
+    const { stderr, stdout } = await exec(command);
+    if (stderr) {
+      return `Something went wrong. Reason -> ${stderr}`;
+    }
+
+    return stdout;
+  },
+};
 
 async function main() {
   const agent = Agent.builder()
     .setInstructions(
       "You are a helpful assistant that can answer questions and help with tasks.",
     )
+    .tool(weatherTool)
+    .tool(cliAccessTool)
     .build();
 
-  const response = agent.getInstruction();
-  console.log(response);
+  agent.attachInterceptor((message) => {
+    const { role, content } = message;
+    let emoji;
+
+    switch (role) {
+      case "assistant":
+        emoji = "🤖";
+        break;
+
+      case "developer":
+        emoji = "💻";
+        break;
+
+      case "user":
+        emoji = "🥸";
+        break;
+
+      default:
+        emoji = "🤔";
+        break;
+    }
+
+    console.info(`${emoji}: ${content}`);
+  });
+
+  const response = await agent.run(
+    "Create a simple hello world program in nodejs in current directory",
+  );
+  // console.log(response);
 }
 
 main();
