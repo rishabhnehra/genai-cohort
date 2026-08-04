61357acd0d50738c0566b979d4cc4cfa432fa845
rishabhnehra <rishabhnehrapersonal@gmail.com>
2026-08-04 20:17:02 +0530

    chore: arranged files

--- apps/agent-sdk/src/index.ts
+++ apps/agent-sdk/src/index.ts
@@ -1,33 +1,5 @@
 import { Agent, type Tool } from "./agent.js";
-import { promisify } from "node:util";
-import { exec as execCallback } from "node:child_process";
-
-const exec = promisify(execCallback);
-
-const weatherTool: Tool = {
-  name: "fetchWeather",
-  description: "Tool that returns weather data of given input string",
-  async executor(city: string) {
-    const result = await fetch(
-      `https://wttr.in/${city.toLowerCase()}?format=%C+%t`,
-    ).then((res) => res.text());
-
-    return JSON.stringify({ city, weather: result });
-  },
-};
-
-const cliAccessTool: Tool = {
-  name: "cliAccess",
-  description: "Access to CLI commands",
-  async executor(command: string) {
-    const { stderr, stdout } = await exec(command);
-    if (stderr) {
-      return `Something went wrong. Reason -> ${stderr}`;
-    }
-
-    return stdout;
-  },
-};
+import { cliAccessTool, weatherTool } from "./tools.js";
 
 async function main() {
   const agent = Agent.builder()
@@ -64,7 +36,7 @@ async function main() {
   });
 
   const response = await agent.run(
-    "Create a simple hello world program in nodejs in current directory",
+    "Create a simple hello world program in cpp in current directory",
   );
   // console.log(response);
 }
